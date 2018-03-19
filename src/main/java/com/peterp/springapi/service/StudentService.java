26614a1f50c1d467205745d2a9b6517e8d567960
Peter P <peter.palocz91@gmail.com>
2018-03-19 11:27:11 +0100

    refactor: refactor exception handling and repository actions

--- src/main/java/com/peterp/springapi/service/StudentService.java
+++ src/main/java/com/peterp/springapi/service/StudentService.java
@@ -4,7 +4,6 @@ import com.peterp.springapi.exceptions.StudentNotFoundException;
 import com.peterp.springapi.repository.StudentRepository;
 import com.peterp.springapi.model.Student;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,7 +13,6 @@ import java.util.Optional;
 public class StudentService {
 
 	@Autowired private StudentRepository studentRepository;
-	@Autowired private MongoTemplate mongoTemplate;
 
 
 	public Student addNewStudent(Student student) {
@@ -22,12 +20,12 @@ public class StudentService {
 	}
 
 	public Student getStudentById(String studentId) {
-		Student student = mongoTemplate.findById(studentId, Student.class);
+		Optional<Student> student = studentRepository.findById(studentId);
 
-		if (student != null) {
-			return student;
+		if (student.isPresent()) {
+			return student.get();
 		} else {
-			throw new StudentNotFoundException("Student is not found with id: " + studentId);
+			throw this.studentNotFound(studentId);
 		}
 	}
 
@@ -36,28 +34,27 @@ public class StudentService {
 	}
 
 	public Student updateStudentById(String studentId, Student newStudent) {
-		Student student = mongoTemplate.findById(studentId, Student.class);
-//		Optional<Student> student = studentRepository.findById(studentId); --> try this. Wont work because of Option <Student>. What can i do to make it work?
+		Optional<Student> student = studentRepository.findById(studentId);
 
-		if (student != null) {
-			student.setCourse(newStudent.getCourse());
-			student.setFirstName(newStudent.getFirstName());
-			student.setLastName(newStudent.getLastName());
+		if (student.isPresent()) {
+			student.get().setCourse(newStudent.getCourse());
+			student.get().setFirstName(newStudent.getFirstName());
+			student.get().setLastName(newStudent.getLastName());
 
-			return this.studentRepository.save(student);
+			return this.studentRepository.save(student.get());
 		} else {
-			throw new StudentNotFoundException("Student is not found with id: " + studentId);
+			throw this.studentNotFound(studentId);
 		}
 
 	}
 
 	public void deleteStudentById(String studentId) {
 		Student student = this.getStudentById(studentId);
-		if (student != null) {
-			this.studentRepository.delete(student);
-		} else {
-			throw new StudentNotFoundException("Student is not found with id: " + studentId);
-		}
+		this.studentRepository.delete(student);
+	}
+
+	private StudentNotFoundException studentNotFound(String studentId) {
+		return new StudentNotFoundException("Student is not found with id: " + studentId);
 	}
 
 }
